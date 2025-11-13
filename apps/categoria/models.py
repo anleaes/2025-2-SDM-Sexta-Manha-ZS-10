8efddd5b5a74676c18ee9b34cf2d016d4b3ec0a4
LarksD <luccadelsantos@gmail.com>
2025-11-13 16:33:10 -0300

    Criação da classe Categoria

--- apps/categoria/models.py
+++ apps/categoria/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Categoria(models.Model):
+    nome = models.CharField('Nome', max_length=50)
+    descricao = models.TextField('Descrição', max_length=100)
+    ativo = models.BooleanField('Ativo', default=True)
+    limite_hospedes = models.IntegerField('Limite de Hóspedes', default=1)
