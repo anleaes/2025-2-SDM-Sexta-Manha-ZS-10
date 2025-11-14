0012bc51ad5a75c549c60f1eaf42bb0990161d1f
LarksD <luccadelsantos@gmail.com>
2025-11-13 22:48:15 -0300

    Criação da class Localizacao no models.py

--- apps/localizacao/models.py
+++ apps/localizacao/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Localizacao(models.Model):
+    cidade = models.CharField('Cidade', max_length=50)
+    estado = models.CharField('Estado', max_length=50)
+    pais = models.CharField('País', max_length=50)
+    cep = models.CharField('CEP', max_length=20)
