ad4631b8ee47ab96501a54e1fc898dfba3102b19
LarksD <luccadelsantos@gmail.com>
2025-11-14 08:53:57 -0300

    Criação da class Anfitriao

--- apps/anfitriao/models.py
+++ apps/anfitriao/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Anfitriao(models.Model):
+    nome = models.CharField('Nome', max_length=50)
+    email = models.CharField('Email', max_length=80)
+    telefone = models.CharField('Telefone', max_length=20)
+    avaliacao_media = models.FloatField('Avaliação Média', default=0.0)
