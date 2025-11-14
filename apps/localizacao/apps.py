115b5c8b3cb63f0011d17f56d772a57a487aa3c1
LarksD <luccadelsantos@gmail.com>
2025-11-13 22:54:52 -0300

    Adiciona o nome legível 'Localização' à configuração da aplicação

--- apps/localizacao/apps.py
+++ apps/localizacao/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class LocalizacaoConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'localizacao'
+    verbose_name = 'Localização'
