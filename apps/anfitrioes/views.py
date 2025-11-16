bdc301e4679015bdc0e48deac3ef546b7e721c25
LarksD <luccadelsantos@gmail.com>
2025-11-16 13:48:06 -0300

    Ajustes finais no views de Anfitriões

--- apps/anfitrioes/views.py
+++ apps/anfitrioes/views.py
@@ -2,5 +2,7 @@ from rest_framework import viewsets
 from .models import Anfitriao
 from .serializer import AnfitriaoSerializer
 
-
 # Create your views here.
+class AnfitriaoViewSet(viewsets.ModelViewSet):
+    queryset = Anfitriao.objects.all()
+    serializer_class = AnfitriaoSerializer
