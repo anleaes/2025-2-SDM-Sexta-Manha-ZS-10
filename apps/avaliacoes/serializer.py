2684bb36854ec5beb7409d2a87fd7f02646323fd
LarksD <luccadelsantos@gmail.com>
2025-11-16 20:03:46 -0300

    Ajustes finais no serializer

--- apps/avaliacoes/serializer.py
+++ apps/avaliacoes/serializer.py
@@ -1,2 +1,7 @@
 from rest_framework import serializers
 from .models import Avaliacao
+
+class AvaliacaoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Avaliacao
+        fields = '__all__'
