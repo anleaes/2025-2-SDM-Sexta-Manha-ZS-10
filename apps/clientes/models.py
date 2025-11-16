5f8280e9564b2d1014c62b3db0883eb444e4f16e
LarksD <luccadelsantos@gmail.com>
2025-11-16 16:17:54 -0300

    Inicio da criacao do models do app cliente

--- apps/clientes/models.py
+++ apps/clientes/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Cliente(models.Model):
+    nome = models.CharField('Nome', max_length=50)
+    email = models.CharField('Email', max_length=100)
+    telefone = models.CharField('Telefone', max_length=20)
+    data_cadastro = models.DateField('Data de Cadastro')
