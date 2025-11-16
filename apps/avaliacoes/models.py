dbed5c5957f82a0496e5fd7f7b9a0dab6dd48492
LarksD <luccadelsantos@gmail.com>
2025-11-16 19:55:57 -0300

    Criação dos models do app Avaliações

--- apps/avaliacoes/models.py
+++ apps/avaliacoes/models.py
@@ -3,3 +3,19 @@ from clientes.models import Cliente
 from hospedagem.models import Hospedagem
 
 # Create your models here.
+class Avaliacao(models.Model):
+    nota = models.IntegerField('Nota')
+    comentario = models.TextField('Comentário', max_length=200)
+    data_avaliacao = models.DateField('Data da Avaliação', auto_now_add=True)
+    publicada = models.BooleanField('Publicada', default=False)
+
+    autor = models.ForeignKey(Cliente, on_delete=models.CASCADE)
+    hospedagem = models.ForeignKey(Hospedagem, on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = 'Avaliação'
+        verbose_name_plural = 'Avaliações'
+        ordering = ['id']
+
+    def __str__(self):
+        return f"Avaliação: {self.nota} - {self.autor.nome} - {self.comentario}"
