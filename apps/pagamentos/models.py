930fb46026f9f501d894d95afd0a622b0e73c3e3
LarksD <luccadelsantos@gmail.com>
2025-11-16 18:04:25 -0300

    Criação inicial do modelo Pagamento com campos para valor, método, status e data

--- apps/pagamentos/models.py
+++ apps/pagamentos/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Pagamento(models.Model):
+    valor = models.FloatField('Valor')
+    metodo_pagamento = models.CharField('Método de Pagamento', max_length=50)
+    status_pagamento = models.CharField('Status do Pagamento', max_length=50)
+    data_pagamento = models.DateField('Data do Pagamento')
