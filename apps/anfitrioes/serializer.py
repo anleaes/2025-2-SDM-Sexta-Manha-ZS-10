e1652849c193188e9a444353bd391feaa7763e00
LarksD <luccadelsantos@gmail.com>
2025-11-16 13:44:56 -0300

    Ajustes finais no serializer de Anfitriões

--- apps/anfitrioes/serializer.py
+++ apps/anfitrioes/serializer.py
@@ -1,2 +1,7 @@
 from rest_framework import serializers
 from .models import Anfitriao
+
+class AnfitriaoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Anfitriao
+        fields = '__all__'
