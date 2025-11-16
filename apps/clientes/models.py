ceddb4f109b9ef01fa0098fce78c87935bcf3e7e
LarksD <luccadelsantos@gmail.com>
2025-11-16 16:20:40 -0300

    Ajustes finais no models

--- apps/clientes/models.py
+++ apps/clientes/models.py
@@ -6,3 +6,11 @@ class Cliente(models.Model):
     email = models.CharField('Email', max_length=100)
     telefone = models.CharField('Telefone', max_length=20)
     data_cadastro = models.DateField('Data de Cadastro')
+    
+    class Meta:
+        verbose_name = 'Cliente'
+        verbose_name_plural = 'Clientes'
+        ordering = ['id']
+
+    def __str__(self):
+        return self.nome
