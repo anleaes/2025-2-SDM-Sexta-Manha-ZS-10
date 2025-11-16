8cbc0a8a45bd2ba7181dfd5c471ced0afa06f79d
LarksD <luccadelsantos@gmail.com>
2025-11-16 20:21:06 -0300

    Criação das views do app Avaliações

--- apps/avaliacoes/views.py
+++ apps/avaliacoes/views.py
@@ -3,3 +3,6 @@ from .models import Avaliacao
 from .serializer import AvaliacaoSerializer
 
 # Create your views here.
+class AvaliacaoViewSet(viewsets.ModelViewSet):
+    queryset = Avaliacao.objects.all()
+    serializer_class = AvaliacaoSerializer
