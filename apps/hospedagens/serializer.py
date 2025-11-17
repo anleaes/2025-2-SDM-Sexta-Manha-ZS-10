dc1e0dce196e41ae0ff4c233307c94f83d9db8c2
LarksD <luccadelsantos@gmail.com>
2025-11-17 08:33:37 -0300

    Ajustes finais no serializer

--- apps/hospedagens/serializer.py
+++ apps/hospedagens/serializer.py
@@ -1,2 +1,7 @@
 from rest_framework import serializers
 from .models import Hospedagem
+
+class HospedagemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Hospedagem
+        fields = '__all__'
