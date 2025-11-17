5efa4432cdf5803cccfe7f8438eaa105afdd7504
LarksD <luccadelsantos@gmail.com>
2025-11-17 08:04:06 -0300

    Importações necessárias

--- apps/hospedagens/models.py
+++ apps/hospedagens/models.py
@@ -1,3 +1,6 @@
 from django.db import models
+from categorias.models import Categoria
+from localizacoes.models import Localizacao
+from anfitrioes.models import Anfitriao
 
 # Create your models here.
