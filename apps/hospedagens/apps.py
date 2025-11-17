65509fc002d04e43c4f8361198ed8da37ebdc2ab
LarksD <luccadelsantos@gmail.com>
2025-11-16 21:44:48 -0300

    Recriação do app hospedagem

--- apps/hospedagens/apps.py
+++ apps/hospedagens/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class HospedagemConfig(AppConfig):
+class HospedagensConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'hospedagem'
+    name = 'hospedagens'
