e44f75eacb3071f42740a0a2e90a048945fe697b
LarksD <luccadelsantos@gmail.com>
2025-11-17 08:55:38 -0300

    Ajustes finais no views

--- apps/hospedagens/views.py
+++ apps/hospedagens/views.py
@@ -3,3 +3,6 @@ from .models import Hospedagem
 from .serializer import HospedagemSerializer
 
 # Create your views here.
+class HospedagemViewSet(viewsets.ModelViewSet):
+    queryset = Hospedagem.objects.all()
+    serializer_class = HospedagemSerializer
