fdc44d3b25de7ec091c13ada7ff3096f397b9a69
LarksD <luccadelsantos@gmail.com>
2025-11-17 08:58:38 -0300

    Criação do URLs de Hospedagens

---
+++ apps/hospedagens/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path, include
+from rest_framework import routers
+from . import views
+
+app_name = 'hospedagens'
+
+router = routers.DefaultRouter()
+router.register('', views.HospedagemViewSet, basename='hospedagens')
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
