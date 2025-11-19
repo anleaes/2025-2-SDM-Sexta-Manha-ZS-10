22a703633cc3d111092ba13ed9a5fd098b8a996c
LarksD <luccadelsantos@gmail.com>
2025-11-19 16:55:36 -0300

    Ajustes finais do views

--- apps/reservas/views.py
+++ apps/reservas/views.py
@@ -3,3 +3,6 @@ from .models import Reserva
 from .serializer import ReservaSerializer
 
 # Create your views here.
+class ReservaViewSet(viewsets.ModelViewSet):
+    queryset = Reserva.objects.all()
+    serializer_class = ReservaSerializer
