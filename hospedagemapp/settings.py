909e0be2b39e2b725b533a0a7697e8deb8874c0d
LarksD <luccadelsantos@gmail.com>
2025-11-20 20:16:36 -0300

    Adição filter

--- hospedagemapp/settings.py
+++ hospedagemapp/settings.py
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
     'avaliacoes.apps.AvaliacoesConfig',
     'hospedagens.apps.HospedagensConfig',
     'reservas.apps.ReservasConfig',
+    'django_filters',
 ]
 
 MIDDLEWARE = [
